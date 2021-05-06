ee84534d27348e198c882910ca71a36e10548fbd
Lorran Mariuba de Oliveira <llorrann@gmail.com>
2021-05-05 23:59:22 -0400

    First commit without consumer.

---
+++ src/util/UpperCaseName.java
@@ -0,0 +1,15 @@
+package util;
+
+import java.util.function.Function;
+
+import entities.Product;
+
+public class UpperCaseName implements Function<Product, String>{
+
+	@Override
+	public String apply(Product t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
